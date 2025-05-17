dbee658237e16d694445f27282097cbbc6fd4597
Ted Lemy <lemyted21@gmail.com>
2025-05-17 16:25:34 -0400

    add quit button, corrected instability bugs

---
+++ src/lib/UserStream.ts
@@ -0,0 +1,180 @@
+const timeslice = 50;
+const interval = 500;
+
+class UserStream {
+    constructor(_mediaSource: MediaSource, _sourceBuffer: SourceBuffer | null, _recorder: MediaRecorder, _mimeType: string)
+    {
+        this.mediaSource = _mediaSource;
+        this.sourceBuffer = _sourceBuffer;
+        this.recorder = _recorder;
+        this.isPause = false;
+        this.mimeType = _mimeType;
+    }
+    mediaSource: MediaSource;
+    sourceBuffer: SourceBuffer | null;
+    recorder: MediaRecorder;
+    isPause: boolean;
+    mimeType: string;
+    device: HTMLVideoElement | HTMLAudioElement |  null = null;
+    intervalId: number = 0;
+
+    setupRecorder()
+    {
+        this.recorder.ondataavailable = async (event) => {
+            if (!this.isPause && event.data.size > 0 && this.sourceBuffer && !this.sourceBuffer.updating)
+            {
+                this.appendToSourceBuffer(await event.data.arrayBuffer());
+            }
+        }
+        this.recorder.start(timeslice);
+    }
+    
+    setLive()
+    {
+        const seekable = this.device?.seekable.end(0);
+
+        if (this.device && seekable && seekable > 0)
+        {
+            const current = this.device?.currentTime;
+            const diff = seekable - current;
+
+            if (diff > 0.3 || this.isPause)
+            {
+                this.device.currentTime = seekable;
+            }
+        }
+    }
+
+    setupBufferCleaner()
+    {
+        this.intervalId = setInterval(() => {
+            try 
+            {   
+                this.setLive();
+            }
+            catch (err)
+            {
+                console.log(err);
+            }
+        }, interval);
+    }
+
+    appendToSourceBuffer(chunk: ArrayBuffer)
+    {
+        if (!this.isPause && !this.sourceBuffer?.updating && this.mediaSource.readyState === 'open')
+        {
+            try
+            {
+                if (this.sourceBuffer)
+                {
+                    this.sourceBuffer?.appendBuffer(chunk);
+                }
+            } 
+            catch (error) 
+            {
+                console.log(error);    
+            }
+        }
+        else
+        {
+            this.pauseStream();
+
+            setTimeout(() => {
+                this.appendToSourceBuffer(chunk)
+            }, interval / 2);
+        }
+    }
+
+    createNewSourceBuffer()
+    {
+        if (this.mediaSource.sourceBuffers.length == 0)
+        {
+            this.sourceBuffer = this.mediaSource.addSourceBuffer(this.mimeType);
+            this.sourceBuffer.mode = 'sequence';
+        }
+    }
+
+    addEventListenerSource()
+    {
+        this.mediaSource.addEventListener('sourceopen', () => {
+            this.createNewSourceBuffer();
+        }, { once: true });
+    }
+
+    pauseStream() 
+    {
+        this.isPause = true;
+        this.recorder.pause();
+    }
+
+    resumeStream()
+    {
+        this.isPause = false;
+        this.recorder.resume();
+    }
+}
+
+export class UserVideoStream extends UserStream
+{
+    setupVideo()
+    {
+        this.addEventListenerSource();
+    
+        this.device = document.querySelector('#camera') as HTMLVideoElement;
+        this.device.src = URL.createObjectURL(this.mediaSource);
+        this.device.play().catch((err) => {});
+
+        this.setupBufferCleaner();
+    }
+
+    init()
+    {
+        try
+        {
+
+            this.setupVideo();
+            this.setupRecorder();
+
+            this.recorder?.addEventListener("error", (e) => {
+                console.error(e);
+            });
+        } 
+        catch (err)
+        {
+            console.log(err);
+        }
+    }
+}
+
+export class UserAudioStream extends UserStream
+{
+    setupAudio()
+    {
+        this.addEventListenerSource();
+        
+        this.device = document.querySelector('#microphone') as HTMLAudioElement;
+        this.device.src = URL.createObjectURL(this.mediaSource);
+        this.device.play().catch((err) => {});
+
+        this.setupBufferCleaner();
+    }
+
+    init()
+    {
+        try
+        {
+
+            this.setupAudio();
+            this.setupRecorder();
+
+            this.recorder?.addEventListener("error", (e) => {
+                console.error(e);
+            });
+        } 
+        catch (err)
+        {
+            console.log(err);
+        }
+    }
+}
+
--- src/routes/rooms/+page.svelte
+++ src/routes/rooms/+page.svelte
@@ -4,19 +4,19 @@
 
     </div>
     <div id="self">
-        <div id="no-camera" style="{!isVideoPause ? "display: none" : ""}">
+        <div id="no-camera" style="{!isVideoPaused ? "display: none" : ""}">
             No camera
         </div>
-        <video id="camera" controlslist="seeking" style="{isVideoPause ? "display: none" : ""}">
+        <video id="camera" controlslist="seeking" style="{isVideoPaused ? "display: none" : ""}">
             <track kind="captions">
         </video>
         <audio id="microphone"></audio>
         <div class="controls">
-            <button id="mic-button" class="control" onclick={(isAudioPause ? resumeAudio : pauseAudio)}>
-                {@html (isAudioPause ? micOff : micOn)}
+            <button id="mic-button" class="control" onclick={(isAudioPaused ? resumeAudio : pauseAudio)}>
+                {@html (isAudioPaused ? micOff : micOn)}
             </button>
-            <button id="cam-button" class="control" onclick={(isVideoPause ? resumeVideo : pauseVideo)}>
-                {@html (isVideoPause ? cameraOff : cameraOn)}
+            <button id="cam-button" class="control" onclick={(isVideoPaused ? resumeVideo : pauseVideo)}>
+                {@html (isVideoPaused ? cameraOff : cameraOn)}
             </button>
             <button id="quit-button" class="control" onclick={quitRoom}>
                 {@html quit}
@@ -78,6 +78,8 @@
 
 <script lang="ts">
     import { onMount } from "svelte";
+    import { UserVideoStream, UserAudioStream } from '$lib/UserStream';
+    import { goto } from '$app/navigation';
     
     const cameraOn = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 -0.5 16 16" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" class="feather feather-video" id="Video--Streamline-Feather" height="16" width="16"><desc>Video Streamline Icon: https://streamlinehq.com</desc><path d="m14.375 4.375 -4.375 3.125 4.375 3.125 0 -6.25z" stroke-width="1"></path><path d="M1.875 3.125h6.875s1.25 0 1.25 1.25v6.25s0 1.25 -1.25 1.25H1.875s-1.25 0 -1.25 -1.25V4.375s0 -1.25 1.25 -1.25" stroke-width="1"></path></svg>`;
     const cameraOff = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 -0.5 16 16" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" class="feather feather-video-off" id="Video-Off--Streamline-Feather" height="16" width="16"><desc>Video Off Streamline Icon: https://streamlinehq.com</desc><path d="M10 10v0.625a1.25 1.25 0 0 1 -1.25 1.25H1.875a1.25 1.25 0 0 1 -1.25 -1.25V4.375a1.25 1.25 0 0 1 1.25 -1.25h1.25m3.5375 0H8.75a1.25 1.25 0 0 1 1.25 1.25v2.0875l0.625 0.625L14.375 4.375v6.25" stroke-width="1"></path><path d="m0.625 0.625 13.75 13.75" stroke-width="1"></path></svg>`;
@@ -85,247 +87,79 @@
     const micOff = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 -0.5 16 16" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mic-off" id="Mic-Off--Streamline-Feather" height="16" width="16"><desc>Mic Off Streamline Icon: https://streamlinehq.com</desc><path d="m0.625 0.625 13.75 13.75" stroke-width="1"></path><path d="M5.625 5.625v1.875a1.875 1.875 0 0 0 3.2 1.3250000000000002M9.375 5.8375V2.5a1.875 1.875 0 0 0 -3.7125000000000004 -0.375" stroke-width="1"></path><path d="M10.625 10.59375A4.375 4.375 0 0 1 3.125 7.5v-1.25m8.75 0v1.25a4.375 4.375 0 0 1 -0.06875 0.76875" stroke-width="1"></path><path d="m7.5 11.875 0 2.5" stroke-width="1"></path><path d="m5 14.375 5 0" stroke-width="1"></path></svg>`;
     const quit = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 -0.5 16 16" fill="none" stroke="#111111" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle" id="X-Circle--Streamline-Feather" height="16" width="16"><desc>X Circle Streamline Icon: https://streamlinehq.com</desc><path d="M1.25 7.5a6.25 6.25 0 1 0 12.5 0 6.25 6.25 0 1 0 -12.5 0" stroke-width="1"></path><path d="m9.375 5.625 -3.75 3.75" stroke-width="1"></path><path d="m5.625 5.625 3.75 3.75" stroke-width="1"></path></svg>`
 
-    const timeslice = 10;
-    const delay = 1000;
-
-    const mimeTypeVideo = 'video/webm; codecs="vp8"'; // TODO might not work for older safari versions
-    const mimeTypeAudio = 'video/webm; codecs="opus"'; // TODO might not work for older safari versions
-    const audioConstraints = {
-        echoCancellation: true,
-        noiseSuppression: true,
-        autoGainControl: true
-    };
-
-    let mediaSourceVideo: MediaSource;
-    let sourceBufferVideo: SourceBuffer | null = null;
-    let recorderVideo: MediaRecorder | null = null;
-    let isVideoPause = $state(false);
-
-    const pauseVideo = () => {
-        recorderVideo?.pause();
-    };
+    let isVideoPaused = $state(false);
+    let pauseVideo = $state(() => { isVideoPaused = true });    
+    let resumeVideo = $state(() => { isVideoPaused = false });
     
-    const resumeVideo = () => {
-        recorderVideo?.resume();
-    };
+    let isAudioPaused = $state(false);
+    let pauseAudio = $state(() => { isAudioPaused = true });
+    let resumeAudio = $state(() => { isAudioPaused = false });
 
-    let mediaSourceAudio: MediaSource;
-    let sourceBufferAudio: SourceBuffer | null = null;
-    let recorderAudio: MediaRecorder | null = null;
-    let isAudioPause = $state(false);
-
-    const pauseAudio = () => {
-        recorderAudio?.pause();
-    };
-
-    const resumeAudio = () => {
-        recorderAudio?.resume();
-    };
-
-    const quitRoom = () => {
-        
-    }
+    let quitRoom = $state(() => { goto("/") });
       
-    onMount(async () => {
+    onMount(main);
+
+    async function main()
+    {
       try {
-        const videoStream = await navigator.mediaDevices.getUserMedia({video: true});
-        mediaSourceVideo = new MediaSource();
-        recorderVideo = new MediaRecorder(videoStream, { mimeType: mimeTypeVideo });
+        const mimeTypeVideo = 'video/webm; codecs="vp8"'; // TODO might not work for older safari versions
+        const mimeTypeAudio = 'audio/webm; codecs="opus"'; // TODO might not work for older safari versions
+        const audioConstraints = { echoCancellation: true, noiseSuppression: true, autoGainControl: true };
 
-        const userVideo = new UserVideoStream(mediaSourceVideo, sourceBufferVideo, recorderVideo, mimeTypeVideo);
-        userVideo.setupVideo();
-        userVideo.setupRecorder();
+        let mediaSourceVideo = new MediaSource();
+        let videoMediaStream = await navigator.mediaDevices.getUserMedia({video: true});
+        let videoMediaRecorder = new MediaRecorder(videoMediaStream, { mimeType: mimeTypeVideo });
 
-        const audioStream = await navigator.mediaDevices.getUserMedia({audio: audioConstraints});
-        mediaSourceAudio = new MediaSource();
-        recorderAudio = new MediaRecorder(audioStream, { mimeType: mimeTypeAudio });
-        
-        const userAudio = new UserAudioStream(mediaSourceAudio, sourceBufferAudio, recorderAudio, mimeTypeAudio);
-        userAudio.setupAudio();
-        userAudio.setupRecorder();
+        let mediaSourceAudio = new MediaSource();
+        let audioMediaStream = await navigator.mediaDevices.getUserMedia({audio: audioConstraints});
+        let audioMediaRecorder = new MediaRecorder(audioMediaStream, { mimeType: mimeTypeAudio });
 
-        recorderVideo?.addEventListener("pause", () => {
-            isVideoPause = true;
-        });
+        let userVideo = new UserVideoStream(mediaSourceVideo, null, videoMediaRecorder, mimeTypeVideo);
+        let userAudio = new UserAudioStream(mediaSourceAudio, null, audioMediaRecorder, mimeTypeAudio);
 
-        recorderVideo?.addEventListener("resume", () => {
-            isVideoPause = false;
-        });
+        pauseVideo = () => { 
+            isVideoPaused = true;
+            userVideo.pauseStream();
+        };    
+        resumeVideo = () => { 
+            isVideoPaused = false;
+            userVideo.resumeStream();
+        };
+        pauseAudio = () => { 
+            isAudioPaused = true;
+            userAudio.pauseStream();
+        };    
+        resumeAudio = () => { 
+            isAudioPaused = false;
+            userAudio.resumeStream();
+        };
 
-        recorderAudio?.addEventListener("pause", () => {
-            isAudioPause = true;
-        });
+        quitRoom = () => {
+            try
+            {
+                userVideo.pauseStream();
+                userVideo.recorder.stop();
+                clearInterval(userVideo.intervalId);
+                userVideo.mediaSource.endOfStream();
 
-        recorderAudio?.addEventListener("resume", () => {
-            isAudioPause = false;
-        });
+                userAudio.pauseStream();
+                userAudio.recorder.stop();
+                clearInterval(userAudio.intervalId);
+                userAudio.mediaSource.endOfStream();
+            } 
+            catch (err)
+            {
+                console.error(err);
+            }
+            goto("/");
+        };
+
+        userVideo.init();
+        userAudio.init();        
       } 
-      catch (err) {
+      catch (err)
+      {
         console.log(err);
       }
-    });
-
-    function getVideoElement()
-    {
-        return document.querySelector('#camera') as HTMLVideoElement;
-    }
-
-    class UserStream {
-        constructor(_mediaSource: MediaSource, _sourceBuffer: SourceBuffer | null, _recorder: MediaRecorder, _mimeType: string)
-        {
-            this.mediaSource = _mediaSource;
-            this.sourceBuffer = _sourceBuffer;
-            this.recorder = _recorder;
-            this.isPause = false;
-            this.mimeType = _mimeType;
-        }
-        mediaSource: MediaSource;
-        sourceBuffer: SourceBuffer | null;
-        recorder: MediaRecorder;
-        isPause: boolean;
-        mimeType: string;
-        device: HTMLVideoElement | HTMLAudioElement |  null = null;
-
-        setupRecorder()
-        {
-            this.recorder.ondataavailable = async (event) => {
-                if (!this.isPause && event.data.size > 0 && this.sourceBuffer && !this.sourceBuffer.updating) {
-                    const buffer = await event.data.arrayBuffer();
-                    this.appendToSourceBuffer(buffer);
-                }
-            }
-    
-            this.recorder.start(timeslice);
-        }
-
-        setupBufferCleaner()
-        {
-            setInterval(() => {
-                if (!this.isPause)
-                {
-                    try 
-                    {   
-                        const seekable = this.device?.seekable.end(0);
-
-                        if (this.device && seekable && seekable > 0)
-                        {
-                            const current = this.device?.currentTime;
-                            const diff = seekable - current;
-
-                            if (seekable > current) 
-                            {
-                                this.mediaSource.setLiveSeekableRange(current, seekable);
-                            }
-
-                            if (diff > 0.25)
-                            {
-                                this.device.currentTime = seekable;
-                            }
-
-                            console.log(seekable);
-                        }
-
-                    }
-                    catch (err)
-                    {
-                        console.log(err);
-                    }
-                }
-            }, delay);
-        }
-
-        appendToSourceBuffer(chunk: ArrayBuffer)
-        {
-            if (!this.isPause && !this.sourceBuffer?.updating && this.mediaSource.readyState === 'open')
-            {
-                try {
-                    if (this.sourceBuffer)
-                    {
-                        this.sourceBuffer?.appendBuffer(chunk);
-                    }
-                } 
-                catch (error) {
-                    console.log(error);    
-                }
-            }
-            else
-            {
-                this.pauseStream();
-
-                setTimeout(() => {
-                    this.appendToSourceBuffer(chunk)
-                }, delay);
-            }
-        }
-
-        createNewSourceBuffer()
-        {
-            for (let i = 0; i < this.mediaSource.sourceBuffers.length; i++)
-            {
-                this.mediaSource.removeSourceBuffer(this.mediaSource.sourceBuffers[0]);
-                this.sourceBuffer  = null;
-                console.log("Clear");
-            }
-
-            console.log("Try add");
-
-            if (this.mediaSource.sourceBuffers.length == 0)
-            {
-                console.log("Start add", this.mediaSource.sourceBuffers.length);
-                this.sourceBuffer = this.mediaSource.addSourceBuffer(this.mimeType);
-                this.sourceBuffer.mode = 'sequence';
-                console.log("New buffer");
-            }
-        }
-
-        addEventListenerSource()
-        {
-            this.mediaSource.addEventListener('sourceopen', () => {
-                console.log('MediaSource ready');
-                this.createNewSourceBuffer();
-            }, { once: true });
-        }
-
-        pauseStream() 
-        {
-            this.isPause = true;
-            this.recorder.pause();
-        }
-
-        resumeStream()
-        {
-            this.isPause = false;
-            this.recorder.resume();
-        }
-    };
-
-    class UserVideoStream extends UserStream
-    {
-        setupVideo()
-        {
-            this.device = getVideoElement();
-            
-            this.addEventListenerSource();
-        
-            this.device.src = URL.createObjectURL(this.mediaSource);
-            this.device.play().catch((err) => {});
-
-            this.setupBufferCleaner();
-        }
-    }
-
-    class UserAudioStream extends UserStream
-    {
-        setupAudio()
-        {
-            this.device = document.querySelector('#microphone') as HTMLAudioElement;
-            
-            this.addEventListenerSource();
-        
-            this.device.src = URL.createObjectURL(this.mediaSource);
-            this.device.play().catch((err) => {});
-
-            this.setupBufferCleaner();
-        }
     }
 </script>
